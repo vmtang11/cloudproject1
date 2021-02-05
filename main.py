b856060532d64dbf60035958bd82e35db181128a
Vanessa Tang <vmtang11@gmail.com>
2021-02-05 05:02:23 +0000

    add pandas and wiki routes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect
-from flask import jsonify
+from flask import Flask, jsonify, redirect
+import pandas as pd
+import wikipedia
 
 app = Flask(__name__)
 
@@ -22,15 +23,30 @@ def html():
     <p><b>World</b></p>
     """
 
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
+
+@app.route('/wikipedia/<company>')
+def wikipedia_route(company):
+
+    # Imports the Google Cloud client library
+    from google.cloud import language
+    result = wikipedia.summary(company, sentences=10)
+
+    client = language.LanguageServiceClient()
+    document = language.Document(
+        content=result,
+        type_=language.Document.Type.PLAIN_TEXT)
+    encoding_type = language.EncodingType.UTF8
+    entities = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type}).entities
+    return str(entities)
+
 @app.route('/weather')
 def weather():
     """Redirects to Durham weather page"""
     return redirect('https://weather.com/weather/tenday/l/Durham+NC?canonicalCityId=63d89211f7bcfd0da57abeac727a32c0f62cb675513c8c4da3432fa06c0a8581')
-
-@app.route('/cloud')
-def cloud():
-    """Redirect to Cloud Computing home page"""
-    return redirect('https://noahgift.github.io/cloud-data-analysis-at-scale/')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
