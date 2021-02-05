07d119d03b662eb13070742494a1901ef4c75fc7
Vanessa Tang <vmtang11@gmail.com>
2021-02-05 01:15:50 +0000

    test cd

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! Welcome to my project 1. And now I can test continuous delivery...'
+    return 'Hello World! Welcome to my project 1. Testing...'
 
 @app.route('/name/<value>')
 def name(value):
